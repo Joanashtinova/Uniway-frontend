11fe7189e11a3ed0b983fe61e376c2d35fc75eab
Joanashtinova <133491667+Joanashtinova@users.noreply.github.com>
2023-05-13 14:23:06 +0300

    [add] working firebase data

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,8 +2,21 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { db } from "@/Global/firebase";
 
 export default function Home() {
+  const [documents, setDocuments] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getDocs(collection(db, "developers"));
+      setDocuments(data.docs.map((doc) => doc.data()));
+      console.log(data.docs);
+    };
+    fetchData();
+  }, []);
   return (
     <>
       <Head>
