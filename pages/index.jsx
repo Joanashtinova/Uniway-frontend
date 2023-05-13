83dd96cba64423ae1938cdbe45db42fc93778230
AnnaAngelova <a.angelova2005@gmail.com>
2023-05-13 12:45:08 +0300

    {add} import Video

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import Home from "@/Components/Home";
 
 export default function Home() {
   return (
