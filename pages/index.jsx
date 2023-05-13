af1b3372b576cabc4fb125a51d7d36e24f83bffe
Joanashtinova <133491667+Joanashtinova@users.noreply.github.com>
2023-05-13 12:11:07 +0300

    [add] new file

---
+++ pages/index.jsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Uniway</title>
+        <meta name="description" content="Find your passion!" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Uniway is coming!!!!</h1>
+      </main>
+    </>
+  );
+}
