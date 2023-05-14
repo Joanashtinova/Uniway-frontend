a5887a06f436936ce73e5679fa01250967796fa7
Joanashtinova <133491667+Joanashtinova@users.noreply.github.com>
2023-05-14 18:34:49 +0300

    [add] design

--- Components/HomeComponent.jsx
+++ Components/HomeComponent.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import styles from "./Global.module.css";
 import Video from "./Video.jsx";
-import Image from "next/image";
 import hero from "../public/images/Untitled_Artwork.PNG";
 import {
-    getFirestore,
-    collection,
-    getDocs,
-    query,
-    limit,
+  getFirestore,
+  collection,
+  getDocs,
+  query,
+  limit,
 } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { db, storage } from "@/Global/firebase";
@@ -17,32 +16,32 @@ import SearchComponent from "@/Components/search";
 import RowComponent from "./RowComponent";
 
 function HomeComponent() {
-    const [videos, setVideos] = useState([]);
+  const [videos, setVideos] = useState([]);
 
-    const fetchData = async () => {
-        const data = await getDocs(query(collection(db, "videos"), limit(4)));
-        setVideos(data.docs.map((doc) => doc.data()));
-        console.log(data.docs);
-        console.log(videos);
-    };
+  const fetchData = async () => {
+    const data = await getDocs(query(collection(db, "videos"), limit(4)));
+    setVideos(data.docs.map((doc) => doc.data()));
+    console.log(data.docs);
+    console.log(videos);
+  };
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    return (
-        <div>
-            <div className={styles.hero_image}>
-                <div className={styles.hero_text}>
-                    <h1>Uniway</h1>
-                    <p>Something is coming...</p>
-                </div>
-            </div>
-            <div className={styles.hero_body}>
-                <RowComponent videos={videos} />
-            </div>
+  return (
+    <div>
+      <div className={styles.hero_image} style={{ backgroundColor: "pink" }}>
+        <div className={styles.hero_text}>
+          <h1>Uniway</h1>
+          <p>Something is coming...</p>
         </div>
-    );
+      </div>
+      <div className={styles.hero_body}>
+        <RowComponent videos={videos} />
+      </div>
+    </div>
+  );
 }
 
 export default HomeComponent;
--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -13,158 +13,147 @@ import AdbIcon from "@mui/icons-material/Adb";
 import Link from "next/link";
 
 const pages = [
-    { name: "Видеа", page: "videos" },
-    { name: "Калкулатор", page: "calculator" },
+  { name: "Видеа", page: "videos" },
+  { name: "Калкулатор", page: "calculator" },
 ];
 
 function Navbar() {
-    const [anchorElNav, setAnchorElNav] = React.useState(null);
-    const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-    const handleOpenNavMenu = (event) => {
-        setAnchorElNav(event.currentTarget);
-    };
-    const handleOpenUserMenu = (event) => {
-        setAnchorElUser(event.currentTarget);
-    };
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
 
-    const handleCloseNavMenu = () => {
-        setAnchorElNav(null);
-    };
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
-    const handleCloseUserMenu = () => {
-        setAnchorElUser(null);
-    };
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
-    return (
-        <AppBar position="static">
-            <Container maxWidth="xl">
-                <Toolbar disableGutters>
-                    <AdbIcon
-                        sx={{ display: { xs: "none", md: "flex" }, mr: 1 }}
-                    />
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="a"
-                        href="/"
-                        sx={{
-                            mr: 2,
-                            display: { xs: "none", md: "flex" },
-                            fontFamily: "monospace",
-                            fontWeight: 700,
-                            letterSpacing: ".3rem",
-                            color: "inherit",
-                            textDecoration: "none",
-                        }}
-                    >
-                        Uniway
-                    </Typography>
+  return (
+    <AppBar
+      position="static"
+      sx={{
+        backgroundColor: "white",
+        borderRadius: "60px",
+        overflow: "hidden",
+      }}
+    >
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Link href="/">
+            <IconButton aria-label="Return to home page">
+              <img
+                src="https://o.remove.bg/downloads/60185352-19a4-43a5-9e55-e1e299bb6870/logo-removebg-preview-removebg-preview.png"
+                alt="Logo"
+                style={{ height: 90, marginRight: -6 }}
+              />
+            </IconButton>
+          </Link>
 
-                    <Box
-                        sx={{
-                            flexGrow: 1,
-                            display: { xs: "flex", md: "none" },
-                        }}
-                    >
-                        <IconButton
-                            size="large"
-                            aria-label="account of current user"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            onClick={handleOpenNavMenu}
-                            color="inherit"
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                        <Menu
-                            id="menu-appbar"
-                            anchorEl={anchorElNav}
-                            anchorOrigin={{
-                                vertical: "bottom",
-                                horizontal: "left",
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: "top",
-                                horizontal: "left",
-                            }}
-                            open={Boolean(anchorElNav)}
-                            onClose={handleCloseNavMenu}
-                            sx={{
-                                display: { xs: "block", md: "none" },
-                            }}
-                        >
-                            {pages.map((page) => (
-                                <MenuItem
-                                    key={page.name}
-                                    onClick={handleCloseNavMenu}
-                                >
-                                    <Link
-                                        href={"/" + page.page}
-                                        style={{
-                                            color: "black",
-                                            textDecoration: "none",
-                                        }}
-                                    >
-                                        <Typography textAlign="center">
-                                            {page.name}
-                                        </Typography>
-                                    </Link>
-                                </MenuItem>
-                            ))}
-                        </Menu>
-                    </Box>
-                    <AdbIcon
-                        sx={{ display: { xs: "flex", md: "none" }, mr: 1 }}
-                    />
-                    <Typography
-                        variant="h5"
-                        noWrap
-                        component="a"
-                        href=""
-                        sx={{
-                            mr: 2,
-                            display: { xs: "flex", md: "none" },
-                            flexGrow: 1,
-                            fontFamily: "monospace",
-                            fontWeight: 700,
-                            letterSpacing: ".3rem",
-                            color: "inherit",
-                            textDecoration: "none",
-                        }}
-                    >
-                        Uniway
-                    </Typography>
-                    <Box
-                        sx={{
-                            flexGrow: 1,
-                            display: { xs: "none", md: "flex" },
-                        }}
-                    >
-                        {pages.map((page) => (
-                            <Button
-                                key={page}
-                                onClick={handleCloseNavMenu}
-                                sx={{ my: 2, color: "white", display: "block" }}
-                            >
-                                <Link
-                                    href={"/" + page.page}
-                                    style={{
-                                        color: "white",
-                                        textDecoration: "none",
-                                    }}
-                                >
-                                    <Typography textAlign="center">
-                                        {page.name}
-                                    </Typography>
-                                </Link>
-                            </Button>
-                        ))}
-                    </Box>
-                </Toolbar>
-            </Container>
-        </AppBar>
-    );
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "flex", md: "none" },
+            }}
+          >
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page.name} onClick={handleCloseNavMenu}>
+                  <Link
+                    href={"/" + page.page}
+                    style={{
+                      color: "black",
+                      textDecoration: "none",
+                    }}
+                  >
+                    <Typography textAlign="center">{page.name}</Typography>
+                  </Link>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            Uniway
+          </Typography>
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "none", md: "flex" },
+            }}
+          >
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                <Link
+                  href={"/" + page.page}
+                  style={{
+                    color: "yellow",
+                    textDecoration: "none",
+                    fontFamily: "Arial, Helvetica, sans-serif",
+                    fontSize: "17px",
+                  }}
+                >
+                  <p textAlign="center">{page.name}</p>
+                </Link>
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 }
 export default Navbar;
