5b925178e200a51e4640968b3b8c0494dafbdb06
Joanashtinova <133491667+Joanashtinova@users.noreply.github.com>
2023-05-15 22:59:29 +0300

    [add] design

--- Components/Video.jsx
+++ Components/Video.jsx
@@ -4,17 +4,17 @@ import styles from "./Global.module.css";
 import Link from "next/link";
 
 function Video(props) {
-    return (
-        <div className={styles.video_tumb}>
-            <Link
-                href={"/videos/" + props.title.toLowerCase()}
-                className={styles.title_link}
-            >
-                <img className={styles.imageContainer} src={props.image} />
-                <h1 className={styles.title}>{props.title}</h1>
-            </Link>
-        </div>
-    );
+  return (
+    <div className={styles.video_tumb}>
+      <Link
+        href={"/videos/" + props.title.toLowerCase()}
+        className={styles.title_link}
+      >
+        <img className={styles.imageContainer} src={props.image} />
+        <h1 className={styles.title}>{props.title}</h1>
+      </Link>
+    </div>
+  );
 }
 
 export default Video;
