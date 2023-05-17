82dde0817be737c1e076b7d4fae9923b5e6a8edd
AnnaAngelova <a.angelova2005@gmail.com>
2023-05-17 14:53:33 +0300

    {add} videos on phone

--- Components/RowComponent.jsx
+++ Components/RowComponent.jsx
@@ -2,29 +2,31 @@ import React from "react";
 import styles from "./Global.module.css";
 import Video from "./Video.jsx";
 import Link from "next/link";
+import { Grid } from "@mui/material";
 
 function RowComponent(props) {
-    return (
-        <div>
-            <div style={{ marginBottom: "20px" }}>
-                <Link href={"/videos"}>See More</Link>
-            </div>
+  return (
+    <div>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={12} style={{ marginBottom: "20px" }}>
+          <Link href={"/videos"}>Виж всички видеа</Link>
+        </Grid>
 
-            <div className={styles.videoContainer}> 
-                {props.videos.map((video) => {
-                    return (
-                        <div>
-                            <Video
-                                title={video.title}
-                                image={video.tumbnail_url}
-                                id={video.id}
-                            />
-                        </div>
-                    );
-                })}
-            </div>
-        </div>
-    );
+        {props.videos.map((video) => {
+          <Grid>className={styles.videoContainer}</Grid>;
+          return (
+            <Grid item xs={6} md={3}>
+              <Video
+                title={video.title}
+                image={video.tumbnail_url}
+                id={video.id}
+              />
+            </Grid>
+          );
+        })}
+      </Grid>
+    </div>
+  );
 }
 
 export default RowComponent;
