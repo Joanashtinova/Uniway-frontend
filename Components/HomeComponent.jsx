030707a452d4c7d17b3ec1eea998a138c4ad6cef
Mitrandir-main <dian.kumanov@yahoo.com>
2023-05-20 08:16:02 +0300

    [add] fix home for phone

--- Components/HomeComponent.jsx
+++ Components/HomeComponent.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import styles from "./Global.module.css";
 import Video from "./Video.jsx";
 import {
-  getFirestore,
-  collection,
-  getDocs,
-  query,
-  limit,
+    getFirestore,
+    collection,
+    getDocs,
+    query,
+    limit,
 } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { db, storage } from "@/Global/firebase";
@@ -15,71 +15,96 @@ import SearchComponent from "@/Components/search";
 import RowComponent from "./RowComponent";
 import Logo from "../public/images/logo.png";
 import Homeimage1 from "./Homeimage1";
-import Nananan from "../public/images/burrrrrr.png";
+import Strasttezt from "../public/images/burrrrrr.png";
 import Image from "next/image";
 import Izgledaj from "../public/images/izgledaj.png";
-import Momiche from "../public/images/image_home.png";
-import blabla from "../public/images/lll.png";
+import genZgirl from "../public/images/lll.png";
 import TextJ from "../public/images/text.png";
+import MomicheClouds from "../public/images/momiche.png";
+import { Grid } from "@mui/material";
 
 function HomeComponent() {
-  const [videos, setVideos] = useState([]);
+    const [videos, setVideos] = useState([]);
 
-  const fetchData = async () => {
-    const data = await getDocs(query(collection(db, "videos"), limit(4)));
-    setVideos(data.docs.map((doc) => doc.data()));
-    console.log(data.docs);
-    console.log(videos);
-  };
+    const fetchData = async () => {
+        const data = await getDocs(query(collection(db, "videos"), limit(4)));
+        setVideos(data.docs.map((doc) => doc.data()));
+        console.log(data.docs);
+        console.log(videos);
+    };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+    useEffect(() => {
+        fetchData();
+    }, []);
 
-  return (
-    <div>
-      <div
-        className={styles.hero_image}
-        style={{
-          backgroundColor: "#adf7b6",
-          height: "800px",
-          width: "100%",
-        }}
-      >
-        <Image className={styles.bla} src={Nananan} width={500} height={200} />
-        <Image
-          className={styles.Momiche}
-          src={Momiche}
-          width={600}
-          height={600}
-        />
-        <h4 className={styles.teeext}>
-          Най-доброто за един млад човек е ... Вдъхновението! Всяко наше видео
-          ви дава точно това.
-        </h4>
-      </div>
-      <div
-        className={styles.hero_body}
-        style={{
-          backgroundColor: "#087e8b",
-          height: "1100px",
-          width: "100%",
-          marginTop: "-5rem",
-        }}
-      >
-        <Image
-          className={styles.izgledaj}
-          src={Izgledaj}
-          width={500}
-          height={150}
-        />
+    return (
+        <div>
+            <Grid
+                container
+                spacing={2}
+                style={{
+                    backgroundColor: "#adf7b6",
+                    // height: "800px",
+                    width: "86%",
+                    margin: "0 auto",
+                    marginTop: "20px",
+                }}
+            >
+                <Grid item xs={12} md={6}>
+                    <Image
+                        className={styles.bla}
+                        src={Strasttezt}
+                        width={500}
+                        height={200}
+                    />
+                    <h4 className={styles.teeext} style={{ marginTop: "20px" }}>
+                        Най-доброто за един млад човек е ... Вдъхновението!
+                        Всяко наше видео ви дава точно това.
+                    </h4>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <Image
+                        className={styles.Momiche}
+                        src={MomicheClouds}
+                        width={600}
+                        height={600}
+                    />
+                </Grid>
+            </Grid>
+            <Grid
+                container
+                spacing={2}
+                style={{
+                    backgroundColor: "#087e8b",
+                    // height: "800px",
+                    // width: "86%",
+                    margin: "0 auto",
+                    marginTop: "20px",
+                }}
+            >
+                <Grid item xs={12} md={6}>
+                    <Image
+                        className={styles.izgledaj}
+                        src={Izgledaj}
+                        width={500}
+                        height={150}
+                    />
+                </Grid>
 
-        <Image className={styles.lll} src={blabla} width={600} height={600} />
-
-        <RowComponent className={styles.videobox} videos={videos} />
-      </div>
-    </div>
-  );
+                <Grid item xs={12} md={6}>
+                    <Image
+                        // className={styles.lll}
+                        src={genZgirl}
+                        width={600}
+                        height={600}
+                    />
+                </Grid>
+                <Grid item xs={12} md={12}>
+                    <RowComponent videos={videos} />
+                </Grid>
+            </Grid>
+        </div>
+    );
 }
 
 export default HomeComponent;
--- Components/RowComponent.jsx
+++ Components/RowComponent.jsx
@@ -1,40 +1,44 @@
 import React from "react";
 import styles from "./Global.module.css";
 import Video from "./Video.jsx";
-import Link from "next/li
+import Link from "next/link";
 import { Grid } from "@mui/material";
 import button from "../public/images/buton.png";
 import Image from "next/image";
 
 function RowComponent(props) {
-  return (
-    <div>
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={12} style={{ marginBottom: "-120px" }}>
-          <Link
-            href={"/videos"}
-            className={styles}
-            style={{ marginTop: "-7rem" }}
-          >
-            <Image src={button} alt="button" width={300} height={200} />
-          </Link>
-        </Grid>
+    return (
+        <div>
+            <Grid container spacing={2}>
+                <Grid item xs={12} md={12} style={{ marginBottom: "-120px" }}>
+                    <Link
+                        href={"/videos"}
+                        className={styles}
+                        style={{ marginTop: "-7rem" }}
+                    >
+                        <Image
+                            src={button}
+                            alt="button"
+                            width={300}
+                            height={200}
+                        />
+                    </Link>
+                </Grid>
 
-        {props.videos.map((video) => {
-          <Grid>className={styles.videoContainer}</Grid>;
-          return (
-            <Grid item xs={6} md={3}>
-              <Video
-                title={video.title}
-                image={video.tumbnail_url}
-                id={video.id}
-              />
+                {props.videos.map((video) => {
+                    return (
+                        <Grid item xs={12} md={3}>
+                            <Video
+                                title={video.title}
+                                image={video.tumbnail_url}
+                                id={video.id}
+                            />
+                        </Grid>
+                    );
+                })}
             </Grid>
-          );
-        })}
-      </Grid>
-    </div>
-  );
+        </div>
+    );
 }
 
 export default RowComponent;
