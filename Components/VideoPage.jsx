f074aa884b0628bdea0f9f1cdf3843f8180d9c58
Mitrandir-main <dian.kumanov@yahoo.com>
2023-06-07 13:32:37 +0300

    [add] fix video page a bit

--- Components/VideoPage.jsx
+++ Components/VideoPage.jsx
@@ -10,48 +10,50 @@ import Video from "@/Components/Video";
 import Article from "./Article";
 
 export default function VideoPage(props) {
-  const [video, setVideo] = useState({});
+    const [video, setVideo] = useState({});
 
-  const fetchData = async (title) => {
-    const q = query(
-      collection(db, "videos"),
-      where("id", "==", title.toLowerCase()),
-      limit(1)
-    );
-    const data = await getDocs(q);
-    const videoData = data.docs.map((doc) => doc.data())[0]; // get the first video
-    setVideo(videoData);
-    console.log(videoData);
-  };
+    const fetchData = async (title) => {
+        const q = query(
+            collection(db, "videos"),
+            where("id", "==", title.toLowerCase()),
+            limit(1)
+        );
+        const data = await getDocs(q);
+        const videoData = data.docs.map((doc) => doc.data())[0]; // get the first video
+        setVideo(videoData);
+        console.log(videoData);
+    };
 
-  useLayoutEffect(() => {
-    //router.query.id is the name from the url
+    useLayoutEffect(() => {
+        //router.query.id is the name from the url
 
-    fetchData(props.id);
-  }, [props.id]);
+        fetchData(props.id);
+    }, [props.id]);
 
-  return (
-    <div style={{ height: "100%" }}>
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={6} style={{ marginTop: "150px" }}>
-          <iframe
-            // className={styles.bubbleContainer}
-            width="1020px"
-            height="680px"
-            src={video.video_url}
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encryptet-media;
+    return (
+        <div style={{ height: "100%" }}>
+            <Grid container spacing={2}>
+                <Grid item xs={12} md={6} style={{ marginTop: "150px" }}>
+                    <iframe
+                        // className={styles.bubbleContainer}
+                        width="100%"
+                        height="350px"
+                        src={video.video_url}
+                        frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encryptet-media;
               gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
-          <h1 style={{ fontFamily: "Arial, sans-serif" }}>{video.title}</h1>
-        </Grid>
+                        allowFullScreen
+                    ></iframe>
+                    <h1 style={{ fontFamily: "Arial, sans-serif" }}>
+                        {video.title}
+                    </h1>
+                </Grid>
 
-        <Grid item xs={12} md={6}>
-          <Article text={video.article_text} />
-        </Grid>
-        <Grid item xs={12} md={6}></Grid>
-      </Grid>
-    </div>
-  );
+                <Grid item xs={12} md={6}>
+                    <Article text={video.article_text} />
+                </Grid>
+                <Grid item xs={12} md={6}></Grid>
+            </Grid>
+        </div>
+    );
 }
