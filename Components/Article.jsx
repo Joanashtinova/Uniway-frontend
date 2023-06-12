de23ba8c19e99695a634bac1e95744395d75f236
Joanashtinova <133491667+Joanashtinova@users.noreply.github.com>
2023-06-12 21:29:29 +0300

    bla

--- Components/Article.jsx
+++ Components/Article.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 import styles from "./Global.module.css";
-import ReactMarkdown from "react-markdown";
+
 import remarkGfm from "remark-gfm";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 
 function Article(props) {
-    let text = props.text?.toString();
+  let text = props.text?.toString();
 
-    return (
-        <div>
-            <div className={styles.bubbleContent}>
-                <ReactMarkdown
-                    className={styles.foo}
-                    children={text}
-                    remarkPlugins={[remarkGfm]}
-                />
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className={styles.bubbleContent}>
+        <ReactMarkdown
+          className={styles.foo}
+          children={text}
+          remarkPlugins={[remarkGfm]}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Article;
