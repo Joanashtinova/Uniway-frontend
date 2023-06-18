cd078c461d51b2c58e86a464520df7ea05cf2996
Mitrandir-main <dian.kumanov@yahoo.com>
2023-06-18 23:37:47 +0300

    [add] fix text and other stuff

--- Components/Article_people.jsx
+++ Components/Article_people.jsx
@@ -5,23 +5,23 @@ import remarkGfm from "remark-gfm";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 
 function Article_people(props) {
-  let text = props.text?.toString();
-  let person_name = props.person_name?.toString();
+    let text = props.text?.toString();
+    let person_name = props.person_name?.toString();
 
-  return (
-    <div>
-      <div className={styles.bubbleCon}>
+    return (
         <div>
-          <h1 className={styles.person_name}>{person_name}</h1>
+            <div className={styles.bubbleCon}>
+                <div>
+                    <h1 className={styles.person_name}>{person_name}</h1>
+                </div>
+                <ReactMarkdown
+                    className={styles.balon}
+                    children={text}
+                    remarkPlugins={[remarkGfm]}
+                />
+            </div>
         </div>
-        <ReactMarkdown
-          className={styles.balon}
-          children={text}
-          remarkPlugins={[remarkGfm]}
-        />
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Article_people;
--- Components/HomeComponent.jsx
+++ Components/HomeComponent.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import styles from "./Global.module.css";
 import Video from "./Video.jsx";
 import {
-  getFirestore,
-  collection,
-  getDocs,
-  query,
-  limit,
+    getFirestore,
+    collection,
+    getDocs,
+    query,
+    limit,
 } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { db, storage } from "@/Global/firebase";
@@ -24,87 +24,94 @@ import MomicheClouds from "../public/images/momiche.png";
 import { Grid } from "@mui/material";
 
 function HomeComponent() {
-  const [videos, setVideos] = useState([]);
-  const fetchData = async () => {
-    const data = await getDocs(query(collection(db, "videos"), limit(4)));
-    setVideos(data.docs.map((doc) => doc.data()));
-    console.log(data.docs);
-    console.log(videos);
-  };
-  useEffect(() => {
-    fetchData();
-  }, []);
-  return (
-    <div>
-      <Grid
-        container
-        spacing={2}
-        style={{
-          backgroundColor: "#adf7b6",
-          // height: "800px",
-          width: "86%",
-          margin: "0 auto",
-          marginTop: "20px",
-        }}
-      >
-        <Grid item xs={12} md={6}>
-          <Image
-            className={styles.bla}
-            src={Strasttezt}
-            width={500}
-            height={200}
-            layout="responsive"
-          />
-          <h4 className={styles.teeext} style={{ marginTop: "20px" }}>
-            Uniway!Без повече стресиращо и хаотично търсене на призванието ви.
-            Тук то само ще ви открие!
-          </h4>
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <Image
-            className={styles.Momiche}
-            src={MomicheClouds}
-            width={600}
-            height={600}
-            layout="responsive"
-          />
-        </Grid>
-      </Grid>
-      <Grid
-        container
-        spacing={2}
-        style={{
-          backgroundColor: "#087e8b",
-          // height: "800px",
-          width: "100%",
-          margin: "0 auto",
-          marginTop: "20px",
-        }}
-      >
-        <Grid item xs={12} md={6}>
-          <div className={styles.DivIzgelaj}>
-            <Image
-              className={styles.izgledaj}
-              src={Izgledaj}
-              layout="responsive"
-            />
-          </div>
-          <Image className={styles.TextJ} src={TextJ} />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <div className={styles.DivGenZgirl}>
-            <Image
-              className={styles.genZgirl}
-              src={genZgirl}
-              layout="responsive"
-            />
-          </div>
-        </Grid>
-        <Grid item xs={12}>
-          <RowComponent videos={videos} />
-        </Grid>
-      </Grid>
-    </div>
-  );
+    const [videos, setVideos] = useState([]);
+    const fetchData = async () => {
+        const data = await getDocs(query(collection(db, "videos"), limit(4)));
+        setVideos(data.docs.map((doc) => doc.data()));
+        console.log(data.docs);
+        console.log(videos);
+    };
+    useEffect(() => {
+        fetchData();
+    }, []);
+    return (
+        <div>
+            <Grid
+                container
+                spacing={2}
+                style={{
+                    backgroundColor: "#adf7b6",
+                    // height: "800px",
+                    width: "86%",
+                    margin: "0 auto",
+                    marginTop: "20px",
+                }}
+            >
+                <Grid item xs={12} md={6}>
+                    <Image
+                        className={styles.bla}
+                        src={Strasttezt}
+                        width={500}
+                        height={200}
+                        layout="responsive"
+                    />
+                    <h4 className={styles.teeext} style={{ marginTop: "20px" }}>
+                        Uniway!Без повече стресиращо и хаотично търсене на
+                        призванието ви. Тук то само ще ви открие!
+                    </h4>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <Image
+                        className={styles.Momiche}
+                        src={MomicheClouds}
+                        width={600}
+                        height={600}
+                        layout="responsive"
+                    />
+                </Grid>
+            </Grid>
+            <Grid
+                container
+                spacing={2}
+                style={{
+                    backgroundColor: "#087e8b",
+                    // height: "800px",
+                    width: "100%",
+                    margin: "0 auto",
+                    marginTop: "20px",
+                }}
+            >
+                <Grid item xs={12} md={6}>
+                    <div className={styles.DivIzgelaj}>
+                        <Image
+                            className={styles.izgledaj}
+                            src={Izgledaj}
+                            layout="responsive"
+                        />
+                    </div>
+                    <h5
+                        className={styles.teeext}
+                        style={{ marginTop: "20px", color: "#adf7b6" }}
+                    >
+                        Най-доброто за един млад човек е ... <br />{" "}
+                        Вдъхновението! <br /> Всяко наше видео ви дава точно
+                        това.{" "}
+                    </h5>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <div className={styles.DivGenZgirl}>
+                        <Image
+                            className={styles.genZgirl}
+                            src={genZgirl}
+                            layout="responsive"
+                        />
+                    </div>
+                </Grid>
+                <Grid item xs={12}>
+                    <RowComponent videos={videos} />
+                </Grid>
+            </Grid>
+        </div>
+    );
 }
 export default HomeComponent;
