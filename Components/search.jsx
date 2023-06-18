d8485e91e13979620e0fedb50a638539353c6764
AnnaAngelova <a.angelova2005@gmail.com>
2023-06-18 16:11:52 +0300

    {enter search button}

--- Components/search.jsx
+++ Components/search.jsx
@@ -5,32 +5,30 @@ import { ButtonGroup, IconButton, TextField } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 
 export default function SearchComponent(props) {
-    const [searchTerm, setSearchTerm] = useState("");
-    const [videos, setVideos] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [videos, setVideos] = useState([]);
 
-    const handelSearch = async () => {
-        const q = query(
-            collection(db, "videos"),
-            where("title", "==", searchTerm)
-        );
-        const querySnapshot = await getDocs(q);
-        setVideos(querySnapshot.docs.map((doc) => doc.data()));
-    };
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      props.handleNewSearch(searchTerm);
+    }
+  };
 
-    return (
-        <div style={{ margin: "20px", marginBottom: "50px" }}>
-            <ButtonGroup variant="outlined" aria-label="outlined button group">
-                <TextField
-                    id="outlined-basic"
-                    label="Title"
-                    variant="outlined"
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                />
-                <IconButton onClick={() => props.handleNewSearch(searchTerm)}>
-                    <SearchIcon />
-                </IconButton>
-            </ButtonGroup>
-        </div>
-    );
+  return (
+    <div style={{ margin: "20px", marginBottom: "50px" }}>
+      <ButtonGroup variant="outlined" aria-label="outlined button group">
+        <TextField
+          id="outlined-basic"
+          label="Title"
+          variant="outlined"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          onKeyDown={handleKeyDown}
+        />
+        <IconButton onClick={() => props.handleNewSearch(searchTerm)}>
+          <SearchIcon />
+        </IconButton>
+      </ButtonGroup>
+    </div>
+  );
 }
