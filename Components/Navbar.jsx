2816151379a2341816f41ed419679cbecd01cf59
Mitrandir-main <dian.kumanov@yahoo.com>
2023-07-04 11:42:12 +0300

    [add] navbar fixes

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -17,132 +17,50 @@ import SearchComponent from "./search";
 import styles from "./Global.module.css";
 
 const pages = [
-  { name: "Видеа", page: "videos" },
-  { name: "Калкулатор", page: "calculator" },
+    { name: "Видеа", page: "videos" },
+    { name: "Калкулатор", page: "calculator" },
 ];
 
 function Navbar() {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-  const localstyles = {
-    navbar: {
-      justifyContent: "space-between",
-      position: "sticky",
-      width: "90%",
-      backgroundColor: "#fff",
-      zIndex: 100,
-      padding: "10px 0",
-      margin: "0 auto",
-      borderRadius: "60px",
-      display: "flex",
-    },
-    logo: {
-      margin: 0,
-      padding: "20px",
-      float: "left",
-      marginTop: "50px",
-    },
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+    const localstyles = {};
 
-    nav: {
-      // border: "3px solid red",
-
-      float: "right",
-      marginRight: "40px",
-      display: "flex",
-      top: "0",
-      alignSelf: "right",
-      marginTop: "8px",
-    },
-    ul: {
-      margin: 0,
-      padding: 0,
-      listStyle: "none",
-    },
-    li: {
-      display: "inline-block",
-      marginLeft: "20px",
-    },
-    a: {
-      color: "black",
-      textDecoration: "none",
-    },
-  };
-
-  return (
-    <div class={styles.container}>
-      <div style={localstyles.navbar}>
-        <div>
-          <Link href="/">
-            <Image src={Logo} alt="Logo" className={styles.lllogo} />
-          </Link>
+    return (
+        <div className={styles.container}>
+            <div className={styles.navbar}>
+                <div>
+                    <Link href="/">
+                        <Image
+                            src={Logo}
+                            alt="Logo"
+                            className={styles.lllogo}
+                        />
+                    </Link>
+                </div>
+                <nav className={styles.nav}>
+                    <Link href={"/videos"} className={styles.videobutton}>
+                        Видеа
+                    </Link>{" "}
+                    <SearchComponent /> {/* Add the SearchComponent here */}
+                </nav>
+            </div>
         </div>
-        <nav style={localstyles.nav}>
-          <Link href={"/videos"} className={styles.videobutton}>
-            Видеа
-          </Link>{" "}
-          <SearchComponent /> {/* Add the SearchComponent here */}
-        </nav>
-      </div>
-      {/* // <AppBar
-        //     position="fixed"
-        //     sx={{
-        //         backgroundColor: "white",
-        //         borderRadius: "60px",
-        //         overflow: "hidden",
-        //         marginTop: "50px",
-        //     }}
-        // >
-        //     <Container maxWidth="xl">
-        //         <Toolbar disableGutters>
-        //             <Link href="/">
-        //                 <Image src={Logo} alt="Logo" width={100} height={70} />
-        //             </Link>
-        //             <Box
-        //                 sx={{
-        //                     flexGrow: 1,
-        //                     display: { xs: "none", md: "flex" },
-        //                 }}
-        //             >
-        //                 {pages.map((page) => (
-        //                     <Button
-        //                         key={page}
-        //                         onClick={handleCloseNavMenu}
-        //                         sx={{ my: 2, color: "white", display: "block" }}
-        //                     >
-        //                         <Link
-        //                             href={"/" + page.page}
-        //                             style={{
-        //                                 color: "#0b3954",
-
-        //                                 textDecoration: "none",
-        //                                 fontFamily: "",
-        //                                 fontSize: "17px",
-        //                             }}
-        //                         >
-        //                             <p textAlign="center">{page.name}</p>
-        //                         </Link>
-        //                     </Button>
-        //                 ))}
-        //             </Box>
-        //         </Toolbar>
-        //     </Container>
-        // </AppBar> */}
-    </div>
-  );
+    );
 }
 export default Navbar;
--- pages/videos/index.jsx
+++ pages/videos/index.jsx
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useState } from "react";
 import { collection, getDocs, query } from "firebase/firestore";
 import { db } from "@/Global/firebase";
@@ -12,78 +11,81 @@ import { useRouter } from "next/router";
 import styles from "../../Components/Global.module.css";
 
 export default function Videos() {
-  const [videos, setVideos] = useState([]);
-  const [filteredVideos, setFilteredVideos] = useState([]);
-  const router = useRouter();
-  const { query } = router;
+    const [videos, setVideos] = useState([]);
+    const [filteredVideos, setFilteredVideos] = useState([]);
+    const router = useRouter();
+    const { query } = router;
 
-  const fetchData = async () => {
-    const data = await getDocs(collection(db, "videos"));
-    setVideos(data.docs.map((doc) => doc.data()));
-  };
+    const fetchData = async () => {
+        const data = await getDocs(collection(db, "videos"));
+        setVideos(data.docs.map((doc) => doc.data()));
+    };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+    useEffect(() => {
+        fetchData();
+    }, []);
 
-  useEffect(() => {
-    const { search } = query;
-    if (search) {
-      const searchTerm = search.toString();
-      const newVideos = videos
-        .filter((x) => x.title !== undefined && x.categories !== undefined)
-        .filter(
-          (x) =>
-            x.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
-            x.categories
-              .join(" ")
-              .toLowerCase()
-              .includes(searchTerm.toLowerCase())
-        );
-      setFilteredVideos(newVideos);
-    } else {
-      setFilteredVideos(videos);
-    }
-  }, [videos, query]);
+    useEffect(() => {
+        const { search } = query;
+        if (search) {
+            const searchTerm = search.toString();
+            const newVideos = videos
+                .filter(
+                    (x) => x.title !== undefined && x.categories !== undefined
+                )
+                .filter(
+                    (x) =>
+                        x.title
+                            .toLowerCase()
+                            .includes(searchTerm.toLowerCase()) ||
+                        x.categories
+                            .join(" ")
+                            .toLowerCase()
+                            .includes(searchTerm.toLowerCase())
+                );
+            setFilteredVideos(newVideos);
+        } else {
+            setFilteredVideos(videos);
+        }
+    }, [videos, query]);
 
+    return (
+        <div
+            style={{
+                backgroundColor: "#83c5d2",
+                top: "0",
 
-
-export default function AllvideosPage() {
-  return (
-
-    <div
-      style={{
-        backgroundColor: "#83c5d2",
-        top: "0",
-
-        position: "absolute",
-      }}
-    >
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={12} style={{ marginTop: "150px" }}>
-          <h5
-            style={{ marginLeft: "30px", fontWeight: "90px", color: "#1d3354" }}
-          >
-            Открии своя/та:
-          </h5>
-          <Link href={"/universities"}>
-            <ButtonUni />
-          </Link>{" "}
-          <Link href={"/specialnosti"}>
-            <ButtonSpec />
-          </Link>
-        </Grid>
-        {filteredVideos.map((video) => (
-          <Grid item xs={12} md={3} key={video.id}>
-            <Video
-              title={video.title}
-              image={video.tumbnail_url}
-              id={video.id}
-            />
-          </Grid>
-        ))}
-      </Grid>
-
-    </div>
-  );
+                position: "absolute",
+            }}
+        >
+            <Grid container spacing={2}>
+                <Grid item xs={12} md={12} style={{ marginTop: "150px" }}>
+                    <h5
+                        style={{
+                            marginLeft: "30px",
+                            fontWeight: "90px",
+                            color: "#1d3354",
+                        }}
+                    >
+                        Открии своя/та:
+                    </h5>
+                    <Link href={"/universities"}>
+                        <ButtonUni />
+                    </Link>{" "}
+                    <Link href={"/specialnosti"}>
+                        <ButtonSpec />
+                    </Link>
+                </Grid>
+                {filteredVideos.map((video) => (
+                    <Grid item xs={12} md={3} key={video.id}>
+                        <Video
+                            title={video.title}
+                            image={video.tumbnail_url}
+                            id={video.id}
+                        />
+                    </Grid>
+                ))}
+            </Grid>
+        </div>
+    );
 }
